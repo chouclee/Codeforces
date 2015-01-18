4f1a4ed6ae569f631e3151abfe7f290835433cb3
chouclee <chouclee@cmu.edu>
2015-01-18 17:38:45 -0500

    initial push

---
+++ Codeforces/src/Theatre_Square/Solution.java
@@ -0,0 +1,36 @@
+package Theatre_Square;
+
+/*
+ * Theatre Square in the capital city of Berland has a rectangular shape with 
+ * the size n × m meters. On the occasion of the city's anniversary, a decision 
+ * was taken to pave the Square with square granite flagstones. Each flagstone 
+ * is of the size a × a.
+
+What is the least number of flagstones needed to pave the Square? It's allowed 
+to cover the surface larger than the Theatre Square, but the Square has to be 
+covered. It's not allowed to break the flagstones. The sides of flagstones 
+should be parallel to the sides of the Square.
+
+Input
+The input contains three positive integer numbers in the first line: n,  m and 
+a (1 ≤  n, m, a ≤ 109).
+
+Output
+Write the needed number of flagstones.
+ */
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int size = scan.nextInt();
+    scan.close();
+    System.out.println(minPiece(a, size)*minPiece(b, size));
+  }
+  
+  private static long minPiece(int width, int size) {
+    return (width%size == 0) ? width/size : width/size + 1; 
+  }
+}
